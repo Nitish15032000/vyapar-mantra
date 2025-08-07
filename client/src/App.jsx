71047b10d84501f058b8fa5a469e60054c1a0489
Nitish kumar <nitish.edu.in@gmail.com>
2025-08-07 21:22:13 +0530

    project setup with tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className=' text-5xl text-blue-500'>Making frontend for the vyapar Mantra Appllication</h1>
+    </>
+  )
+}
+
+export default App
